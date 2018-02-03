4a7469d84e50dc40f973b47b2ca830b11ef2a4ed
wenwend <anita.dww@gmail.com>
2018-02-02 17:43:17 -0800

    random test adding two new files: testme.c makefile.

---
+++ projects/dongwen/quiz/testme.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    char c;
+    int min=32, max=126;
+    c=rand()%(max-min+1) + min;
+    return c;
+}
+
+char *inputString()
+{
+    static char charset[]="reset";
+    static int length=5;
+
+    char* randomString=NULL;
+    int i;
+    int randomLength = rand()%length+1;
+    
+    if(randomLength){
+	randomString=malloc(sizeof(char)*(randomLength + 1));	
+    }
+	if(randomString){
+		for(i=0;i<randomLength;i++){
+			int key = rand()%(int)(sizeof(charset)-1);
+			randomString[i]=charset[key];
+		}
+		randomString[randomLength] ='\0';
+	}
+    return randomString;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
